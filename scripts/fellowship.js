228092771572a434e029e3aa966bfbf2c0b195fc
Tony Shifflett <tonyshifflett@tonys-mbp.lan>
2021-04-09 08:27:51 -0500

    Chapter 7 complete - Strider is changed to Aragorn

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -178,6 +178,8 @@ const beautifulStranger = () => {
 
   // 1. change the buddy 'Strider' textnode to "Aragorn"
      // hint: You can get a list of elements by tag name, such as 'aside'
+     $viggo = $('.buddy:nth-of-type(4)')
+     $viggo.text('Aragorn')
 
 };
 
